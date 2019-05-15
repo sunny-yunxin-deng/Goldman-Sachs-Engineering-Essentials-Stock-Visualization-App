04a9e8d91734cbb797fe33b7779f41733012a970
Yunxin Deng <yunxindeng@Yunxins-MacBook-Pro.local>
2019-05-15 16:09:47 -0400

    re-merge

---
+++ techCurriculum/ui/starter_code/cards-app/src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <div>
+        <h1>Cards</h1>
+        <h2>Share your ideas</h2>
+    </div>
+);
+}
+
+export default Title;
